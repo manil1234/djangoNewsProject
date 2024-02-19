c1be7853ff42dea021cab1618f1bc907064f33ef
Manil Kohombage <ed19mk3@leeds.ac.uk>
2024-02-19 16:08:19 +0000

    Client can logout and loops functions

--- client.py
+++ client.py
@@ -6,19 +6,24 @@ def login(username, password):
     url = BASE_URL + 'login/'
     data = {'username': username, 'password': password}
     response = requests.post(url, data=data)
-    if response.status_code == 200:
-        print(response.text)
-    else:
-        print(response.text)
+    print(response.text)
+    
+def perform_logout():
+    url = BASE_URL + 'logout/'
+    response = requests.post(url)
+    print(response.text)
 
 def main():
-    command = input('Enter command (login): ')
-    if command.lower() == 'login':
-        username = input('Enter username: ')
-        password = input('Enter password: ')
-        login(username, password)
-    else:
-        print('Invalid command')
+    while True:
+        command = input('Enter command (login, logout): ')
+        if command.lower() == 'login':
+            username = input('Enter username: ')
+            password = input('Enter password: ')
+            login(username, password)
+        elif command.lower() == 'logout':
+            perform_logout()
+        else:
+            print('Invalid command')
 
 if __name__ == '__main__':
     main()
