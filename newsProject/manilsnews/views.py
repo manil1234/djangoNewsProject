a25939cd9d630f6c0b0803e73462622b990b6902
Manil Kohombage <ed19mk3@leeds.ac.uk>
2024-02-19 16:07:48 +0000

    Working on Logout

--- newsProject/manilsnews/views.py
+++ newsProject/manilsnews/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 @csrf_exempt  # Disable CSRF for this view
-def login(request):
+def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -12,9 +13,19 @@ def login(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             # Authentication successful
-            return HttpResponse('Welcome back!')  # Return 200 OK with welcome message
+            login(request, user)
+            return HttpResponse('Welcome back ' + username + '!')  # Return 200 OK with welcome message
         else:
             # Authentication failed
             return HttpResponse('Login failed', status=401)  # Return 401 Unauthorized with error message
     else:
         return HttpResponse('Method Not Allowed', status=405)  # Return 405 Method Not Allowed for non-POST requests
+
+@csrf_exempt  # Disable CSRF for this view
+# @login_required
+def logout_view(request):
+    try:
+        logout(request)  # Log out the user
+        return HttpResponse('Logged out successfully.')  # Return 200 OK with success message
+    except:
+        return HttpResponse('Not logged in', status=401)  # Return 401 Unauthorized if user is not logged in
--- newsProject/newsProject/urls.py
+++ newsProject/newsProject/urls.py
@@ -20,5 +20,6 @@ from manilsnews import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
