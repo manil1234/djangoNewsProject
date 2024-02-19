7c6a07807b02415108ee5aaa934e01785be62559
Manil Kohombage <ed19mk3@leeds.ac.uk>
2024-02-19 12:42:59 +0000

    Created User Model

--- newsProject/newsProject/settings.py
+++ newsProject/newsProject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'manilsnews.apps.ManilsnewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
